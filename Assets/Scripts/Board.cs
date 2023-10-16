2ac34dd249a4a4915622dc22524f333d8b1ccc5e
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-10-15 20:30:14 -0700

    [update] Board.cs

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
+    // an array of supported keys for entering letters.
     private static readonly KeyCode[] SUPPORTED_KEYS = new KeyCode[] {
         KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F,
         KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L,
@@ -12,15 +13,16 @@ public class Board : MonoBehaviour
         KeyCode.Y, KeyCode.Z,
     };
 
-    private Row[] rows;
+    private Row[] rows; // an array of rows in the game board.
 
-    private string[] solutions;
-    private string[] validWords;
-    private string word;
+    private string[] solutions; // an array of valid solutions.
+    private string[] validWords; // an array of valid words.
+    private string word; // the current target word to guess.
 
-    private int rowIndex;
-    private int columnIndex;
+    private int rowIndex; // the current row index.
+    private int columnIndex; // the current column index for letter input.
 
+    // UI elements for different tile states.
     [Header("States")]
     public Tile.State emptyState;
     public Tile.State occupiedState;
@@ -28,6 +30,7 @@ public class Board : MonoBehaviour
     public Tile.State wrongSpotState;
     public Tile.State incorrectState;
 
+    // UI elements for displaying information.
     [Header("UI")]
     public TextMeshProUGUI invalidWordText;
     public Button newWordButton;
@@ -35,30 +38,33 @@ public class Board : MonoBehaviour
 
     private void Awake()
     {
-        rows = GetComponentsInChildren<Row>();
+        rows = GetComponentsInChildren<Row>(); // get all the Row components in the game board.
     }
 
     private void Start()
     {
-        LoadData();
-        NewGame();
+        LoadData(); // load valid words and solutions from resources.
+        NewGame(); // start a new game.
     }
 
+    // start a new game by clearing the board and selecting a random word.
     public void NewGame()
     {
         ClearBoard();
         SetRandomWord();
 
-        enabled = true;
+        enabled = true; // enable input for letter selection.
     }
 
+    // clear the board to restart the current row.
     public void TryAgain()
     {
         ClearBoard();
-        
-        enabled = true;
+
+        enabled = true; // enable input for letter selection.
     }
 
+    // load valid words and solutions from resource files.
     private void LoadData()
     {
         TextAsset textFile = Resources.Load("official_wordle_all") as TextAsset;
@@ -68,35 +74,40 @@ public class Board : MonoBehaviour
         solutions = textFile.text.Split('\n');
     }
 
+    // select a random word from the available solutions.
     private void SetRandomWord()
     {
         word = solutions[Random.Range(0, solutions.Length)];
         word = word.ToLower().Trim();
     }
+
     private void Update()
     {
-        Row currentRow = rows[rowIndex];
+        Row currentRow = rows[rowIndex]; // get the current row.
 
+        // handle user input for letter selection and clearing.
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
             columnIndex = Mathf.Max(columnIndex - 1, 0);
             currentRow.tiles[columnIndex].SetLetter('\0');
-            currentRow.tiles[columnIndex].SetState(emptyState);  
+            currentRow.tiles[columnIndex].SetState(emptyState);
 
-            invalidWordText.gameObject.SetActive(false);
+            invalidWordText.gameObject.SetActive(false); // hide invalid word message.
         }
-
         else if (columnIndex >= currentRow.tiles.Length)
         {
-            if (Input.GetKeyDown(KeyCode.Return)) {
-                SubmitRow(currentRow);
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SubmitRow(currentRow); // submit the current row.
             }
         }
         else
         {
+            // Handle letter input.
             for (int i = 0; i < SUPPORTED_KEYS.Length; i++)
             {
-                if (Input.GetKeyDown(SUPPORTED_KEYS[i])) {
+                if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
+                {
                     currentRow.tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
                     currentRow.tiles[columnIndex].SetState(occupiedState);
                     columnIndex++;
@@ -104,19 +115,20 @@ public class Board : MonoBehaviour
                 }
             }
         }
-        
     }
 
+    // submit the current row for evaluation.
     private void SubmitRow(Row row)
     {
         if (!IsValidWord(row.word))
         {
-            invalidWordText.gameObject.SetActive(true);
+            invalidWordText.gameObject.SetActive(true); // Show invalid word message.
             return;
         }
 
         string remaining = word;
 
+        // evaluate correctness of letter placements.
         for (int i = 0; i < row.tiles.Length; i++)
         {
             Tile tile = row.tiles[i];
@@ -146,7 +158,7 @@ public class Board : MonoBehaviour
 
                     int index = remaining.IndexOf(tile.letter);
                     remaining = remaining.Remove(index, 1);
-                    remaining = remaining.Insert(index, " "); 
+                    remaining = remaining.Insert(index, " ");
                 }
                 else
                 {
@@ -155,34 +167,21 @@ public class Board : MonoBehaviour
             }
         }
 
-        if (HasWon(row)) {
-            enabled = false;
+        if (HasWon(row))
+        {
+            enabled = false; // disable input if the row is correct.
         }
-
-
-        // for (int i = 0; i < row.tiles.Length; i++)
-        // {
-        //     Tile tile = row.tiles[i];
-
-        //     if (tile.letter == word[i]) {
-        //         tile.SetState(correctState);
-
-        //     } else if (word.Contains(tile.letter)) {
-        //         tile.SetState(wrongSpotState);
-
-        //     } else {
-        //         tile.SetState(incorrectState);
-        //     }
-        // }
 
         rowIndex++;
         columnIndex = 0;
 
-        if (rowIndex >= rows.Length) {
-            enabled = false;
+        if (rowIndex >= rows.Length)
+        {
+            enabled = false; // disable input after reaching the last row.
         }
     }
 
+    // clear the entire game board.
     private void ClearBoard()
     {
         for (int row = 0; row < rows.Length; row++)
@@ -198,11 +197,13 @@ public class Board : MonoBehaviour
         columnIndex = 0;
     }
 
+    // check if the submitted word is valid.
     private bool IsValidWord(string word)
     {
         for (int i = 0; i < validWords.Length; i++)
         {
-            if (validWords[i] == word) {
+            if (validWords[i] == word)
+            {
                 return true;
             }
         }
@@ -210,11 +211,13 @@ public class Board : MonoBehaviour
         return false;
     }
 
+    // check if the row has been successfully completed.
     private bool HasWon(Row row)
     {
         for (int i = 0; i < row.tiles.Length; i++)
         {
-            if (row.tiles[i].state != correctState) {
+            if (row.tiles[i].state != correctState)
+            {
                 return false;
             }
         }
