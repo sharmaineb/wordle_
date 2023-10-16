2e6a39034e3923065269a4431b86deecfdd7d256
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-10-15 20:27:11 -0700

    [update] Row.cs

--- Assets/Scripts/Row.cs
+++ Assets/Scripts/Row.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
+// represents a row of tiles in the wordle puzzle game.
 public class Row : MonoBehaviour
 {
+    // an array of Tile objects that make up the row.
     public Tile[] tiles { get; private set; }
 
+    // gets the word formed by the letters on the tiles in the row.
     public string word
     {
         get
         {
             string word = "";
 
-            for (int i = 0; i < tiles.Length; i++) {
+            for (int i = 0; i < tiles.Length; i++)
+            {
                 word += tiles[i].letter;
             }
 
@@ -18,8 +22,10 @@ public class Row : MonoBehaviour
         }
     }
 
+    // awake is called when the script instance is being loaded.
     private void Awake()
     {
         tiles = GetComponentsInChildren<Tile>();
     }
 }
+
